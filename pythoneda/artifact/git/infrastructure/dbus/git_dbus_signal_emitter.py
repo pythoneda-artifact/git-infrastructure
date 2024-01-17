6fe4bfaf685508e65b4b43cde4c6eb671f00702e
rydnr <github@acm-sl.org>
2024-01-17 19:11:36 +0100

    Added vim and emacs settings

--- pythoneda/artifact/git/infrastructure/dbus/git_dbus_signal_emitter.py
+++ pythoneda/artifact/git/infrastructure/dbus/git_dbus_signal_emitter.py
@@ -61,3 +61,11 @@ class GitDbusSignalEmitter(DbusSignalEmitter):
         result[key] = [DbusChangeStagingCodeDescribed, BusType.SYSTEM]
 
         return result
+# vim: syntax=python ts=4 sw=4 sts=4 tw=79 sr et
+# Local Variables:
+# mode: python
+# python-indent-offset: 4
+# tab-width: 4
+# indent-tabs-mode: nil
+# fill-column: 79
+# End:
